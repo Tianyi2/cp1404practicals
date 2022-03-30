99217f4b63a7df155f06e01220a8a8e549553d58
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-03-30 13:34:12 +0800

    Add check result function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,14 +2,26 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    result = check_result(score)
+    print(result)
+    score = random.randint(0, 100)
+    print(f"A random score of {score} has a result of {check_result(score)}")
+
+
+def check_result(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
