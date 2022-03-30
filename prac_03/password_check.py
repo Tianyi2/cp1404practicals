733ec2932029e84495e48787b40638b3d937934a
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-03-30 10:04:42 +0800

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,18 @@
-MIN_LEN = 9
-password = input("What is ur password?: ")
+def main():
+    MIN_LENGTH = 9
+    password = get_password(MIN_LENGTH)
+    print_asterisk(password)
 
-while len(password) < MIN_LEN:
+
+def get_password(MIN_LENGTH):
     password = input("What is ur password?: ")
+    while len(password) < MIN_LENGTH:
+        password = input("What is ur password?: ")
+    return password
 
-for i in range(len(password)):
-    print("*", end="")
 
+def print_asterisk(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+main()
