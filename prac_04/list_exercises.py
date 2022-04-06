539d570879f0ad75372a234d9c5bf311725d1ca7
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-04-06 16:04:22 +0800

    Add functions to ask username

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,9 +1,17 @@
 numbers = []
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+
+username = input("What is your username?: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
 
 for i in range(5):
     number = int(input("Number: "))
     numbers.append(number)
-
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
