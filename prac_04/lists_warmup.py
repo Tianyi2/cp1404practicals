d20a6645a1a658dcbf0dd75af87cadc5a1cb1160
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-04-06 09:04:39 +0800

    Answer first question

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,5 +11,16 @@ numbers[3:4] -- [1]
 numbers + [6, 5, 3] -- [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 """
 
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
+print(numbers)
+
+# 2. Change the last element of numbers to 1
+
+
+# 3. Get all the elements from numbers except the first two (slice)
+
+
+# 4. Check if 9 is an element of numbers
 
 
