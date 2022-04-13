96ac61de884a96a03908f0718bc997c57fd5303d
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-04-13 10:00:12 +0800

    Add colour to hexadecimal colour dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+COLOUR_TO_HEXADECIMAL_COLOUR = {"AliceBlue": "#f0f8ff", "Amaranth": "Amaranth", "Amber": "#ffbf00",
+                                "Amethyst": "#9966cc", "AntiqueWhite": "#faebd7", "Apricot": "#fbceb1",
+                                "Aqua": "#00ffff", "Asparagus": "#87a96b", "Aureolin": "#fdee00", "Azure1": "#f0ffff"}
