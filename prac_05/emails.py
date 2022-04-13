b7eb9bf29d7c56646d0f9bb4f7d4d0961fdb04da
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-04-13 12:01:11 +0800

    Add function for user to correct his name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,6 +3,11 @@ def main():
     email = input("Email: ")
     while email != "":
         name = get_name(email)
+        check_name = input("Is your name {}? (Y/n) ".format(name))
+        if check_name.upper() != "Y" and check_name != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
 
 
 def get_name(email):
