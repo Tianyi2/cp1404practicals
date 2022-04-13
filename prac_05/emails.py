ef0b7ba9920a159dee6d7aa8ac849ddec8b8fab7
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-04-13 11:59:08 +0800

    Add function to get name from email address

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,15 @@
 def main():
     email_to_name = {}
     email = input("Email: ")
+    while email != "":
+        name = get_name(email)
+
+
+def get_name(email):
+    fullname = email.split('@')[0]
+    part_of_name = fullname.split('.')
+    name = " ".join(part_of_name).title()
+    return name
+
 
 main()
