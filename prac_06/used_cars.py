9f7049d9e7357ef4fea2d41299b8201edadd05a2
Zhang <tianyi.zhang@my.jcu.edu.au>
2022-04-28 10:08:18 +0800

    Complete Modifications questions

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, "my_car")
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -14,8 +14,20 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
-
+    print(f"Car {{{my_car.fuel}}} {{{my_car.odometer}}}")
 
 main()
 
-limo = Car(100)
+# 1.
+limo = Car(fuel=100, name="limo")
+# 2.
+limo.add_fuel(20)
+# 3.
+print(f"Fuel is {limo.fuel}")
+# 4.
+limo.drive(115)
+# 5.
+print(f"Odometer is {limo.odometer}")
+# 6.
+print(limo)
+
